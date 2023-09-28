3b1d4e523a3085297cfbf7841c49f7411a54957b
AlanFeng <alanfeng528@gmail.com>
2023-09-28 15:05:00 +1300

    create the file

--- src/StudentSystem.java
+++ src/StudentSystem.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StudentSystem {
 
+
+
     public static void main(String[] args) {
+
+        ArrayList <Student> list = new ArrayList<>();
+
+
 
        while (true){
            System.out.println("Welcome to the Student Manager System");
@@ -16,10 +24,10 @@ public class StudentSystem {
            String choose = sc.next();
 
            switch (choose){
-               case "1"-> addStudent();
-               case "2"-> deleteStudent();
-               case "3"-> modifyStudent();
-               case "4"-> searchStudent();
+               case "1"-> addStudent(list);
+               case "2"-> deleteStudent(list);
+               case "3"-> modifyStudent(list);
+               case "4"-> searchStudent(list);
 
                case "5"-> {
                    System.out.println("Exit");
@@ -37,20 +45,50 @@ public class StudentSystem {
 
 
 
-    public static void addStudent(){
-        System.out.println("Add student");
+    public static void addStudent(ArrayList <Student> list){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter student id");
+        String id = sc.next();
+
+        System.out.println("Please enter student name");
+        String name = sc.next();
+        System.out.println("Please enter student age");
+        int age = sc.nextInt();
+        System.out.println("Please enter student address");
+        String address = sc.next();
+
+        Student s = new Student(id,name,age,address);
+        list.add(s);
+
 
     }
-    public static void deleteStudent(){
+    public static void deleteStudent(ArrayList <Student> list){
         System.out.println("Delete student");
 
     }
-    public static void modifyStudent(){
+    public static void modifyStudent(ArrayList <Student> list){
         System.out.println("Modify student");
 
     }
-    public static void searchStudent(){
+    public static void searchStudent(ArrayList <Student> list){
         System.out.println("Search student");
+
+        if (list.isEmpty()){
+            System.out.println("There is no student info, please added");
+            return;
+        }else {
+
+            System.out.println("ID\t\tName\t\tAge\t\tAddress");
+            for (Student student : list) {
+                System.out.println(student.getId() + "\t" + student.getId() + "\t" + student.getName() + "\t" + student.getAddress() + "\t");
+
+            }
+
+        }
+
+
 
     }
 
